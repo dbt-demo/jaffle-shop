a07018cf86f0182f1b8a4a9c644edc2d07283d43
Diana <diana@artemisdata.io>
2024-06-17 16:09:51 +0000

    Refactor orders.sql to remove CTEs and simplify query structure

--- models/orders.sql
+++ models/orders.sql
@@ -1,56 +1,29 @@
 {% set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] %}
 
-with orders as (
+select
+    orders.order_id,
+    orders.customer_id,
+    orders.order_date,
+    orders.status,
 
-    select * from {{ ref('stg_orders') }}
+    {% for payment_method in payment_methods -%}
 
-),
+    order_payments.{{ payment_method }}_amount,
 
-payments as (
+    {% endfor -%}
 
-    select * from {{ ref('stg_payments') }}
+    order_payments.total_amount as amount
 
-),
+from {{ ref('stg_orders') }} as orders
 
-order_payments as (
-
+left join (
     select
         order_id,
-
         {% for payment_method in payment_methods -%}
         sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount,
         {% endfor -%}
-
         sum(amount) as total_amount
-
-    from payments
-
+    from {{ ref('stg_payments') }}
     group by order_id
-
-),
-
-final as (
-
-    select
-        orders.order_id,
-        orders.customer_id,
-        orders.order_date,
-        orders.status,
-
-        {% for payment_method in payment_methods -%}
-
-        order_payments.{{ payment_method }}_amount,
-
-        {% endfor -%}
-
-        order_payments.total_amount as amount
-
-    from orders
-
-
-    left join order_payments
-        on orders.order_id = order_payments.order_id
-
-)
-
-select * from final
+) as order_payments
+on orders.order_id = order_payments.order_id
