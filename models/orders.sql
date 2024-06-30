2596cb0890c6737ff7ad9ff6b4f08749cd831821
Diana <diana@artemisdata.io>
2024-06-30 03:43:32 +0000

    Revert addition of orders and payment aggregation in orders.sql

--- models/orders.sql
+++ models/orders.sql
@@ -1,4 +1,25 @@
 {% set payment_methods = ['credit_card', 'coupon', 'bank_transfer', 'gift_card'] %}
 
-
+with orders as (
+    select *
+    from {{ ref('stg_orders') }}
+),
+order_payments as (
+    select
+        order_id,
+        count(*) as number_of_payments
+    from {{ ref('stg_payments') }}
+    group by order_id
+),
+final as (
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        orders.status,
+        order_payments.number_of_payments
+    from orders
+    left join order_payments
+    on orders.order_id = order_payments.order_id
+)
 select * from final
