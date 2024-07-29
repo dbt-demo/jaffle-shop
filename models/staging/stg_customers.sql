84926ffd517b60498461d4bddaecd5bd31b54e5c
Diana <diana@artemisdata.io>
2024-07-29 22:08:26 +0000

    Simplify SQL models by removing unnecessary CTEs

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,27 +1,10 @@
 {{ config(materialized='incremental', unique_key='customer_id') }}
 
-with source as (
-
-    {#-
-    Normally we would select from the table here, but we are using seeds to load
-    our data in this project
-    #}
-    select * from {{ ref('raw_customers') }}
-
-),
-
-renamed as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from source
-
-)
-
-select * from renamed
+select
+    id as customer_id,
+    first_name,
+    last_name
+from {{ ref('raw_customers') }}
 
 {% if is_incremental() %}
   where customer_id > (select coalesce(max(customer_id), 0) from {{ this }})
--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,28 +1,11 @@
 {{ config(materialized='incremental', unique_key='order_id') }}
 
-with source as (
-
-    {#-
-    Normally we would select from the table here, but we are using seeds to load
-    our data in this project
-    #}
-    select * from {{ ref('raw_orders') }}
-
-),
-
-renamed as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from source
-
-)
-
-select * from renamed
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+from {{ ref('raw_orders') }}
 
 {% if is_incremental() %}
   where order_id > (select coalesce(max(order_id), 0) from {{ this }})
