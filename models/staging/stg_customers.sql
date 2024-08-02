f1b806c6fae1e24823c16d45a11ecaa8c7828844
Diana <diana@artemisdata.io>
2024-08-02 23:46:03 +0000

    Remove renamed CTE and use select * from source in stg_customers model

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -8,15 +8,6 @@ with source as (
 
 ),
 
-renamed as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from source
-
-)
+SELECT * FROM source
 
 select * from renamed
