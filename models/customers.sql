749b47dbb6d018a308a1c6b44d248cd9a430d0a7
Diana <diana@artemisdata.io>
2024-08-02 19:13:00 +0000

    Replace wildcard selector with explicit columns in orders CTE from stg_orders

--- models/customers.sql
+++ models/customers.sql
@@ -6,7 +6,12 @@ with customers as (
 
 orders as (
 
-    select * from {{ ref('stg_orders') }}
+    select
+        order_id,
+        customer_id,
+        order_date,
+        status
+    from {{ ref('stg_orders') }}
 
 ),
 
